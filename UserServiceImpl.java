ae4e978213b4813038a8eeea3a915ba727cb4409
NotShin <nhatnam050214@gmail.com>
2025-08-28 15:38:58 +0700

    Add files via upload

---
+++ UserServiceImpl.java
@@ -0,0 +1,22 @@
+package Service;
+
+import Dao.UserDao;
+import Dao.UserDaoImpl;
+import Model.User;
+
+public class UserServiceImpl implements UserService {
+	UserDao userDao = new UserDaoImpl();
+	
+	@Override
+	public User login(String username, String password) {
+			User user = this.get(username);
+	if (user != null && password.equals(user.getPassWord())) {
+			return user;
+	}
+	return null;
+	}
+	@Override
+	public User get(String username) {
+		return userDao.get(username);
+	}
+}
